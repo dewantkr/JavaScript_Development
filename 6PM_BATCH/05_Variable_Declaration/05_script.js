0f488d169b63cc2689f7399708258dceeb39b618
NAVEEN SAGGAM <thenaveensaggam@gmail.com>
2018-08-21 19:20:42 +0530

    added

---
+++ 6PM_BATCH/05_Variable_Declaration/05_script.js
@@ -0,0 +1,29 @@
+// Create two variables and display their sum
+var a = 10;
+var b = 20;
+var sum = a + b;
+console.log("The Sum of a , b is : " + sum);
+
+// Find the biggest value between 2 numbers
+var p = 10;
+var q = 20;
+if(p > q){
+    console.log('The Big value is : ' + p);
+}
+else{
+    console.log('The Big Value is : ' + q);
+}
+
+// Find the biggest value among 3 numbers
+var x = 10;
+var y = 20;
+var z = 30;
+if(x >y && x > z){
+    console.log('The Biggest value is : ' + x);
+}
+else if(y > z){
+    console.log('The Biggest value is : ' + y);
+}
+else{
+    console.log('The Biggest value is : ' + z);
+}
